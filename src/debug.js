f2dea15a900040c63e83180b055dcd0c7220731a
developerbello <developerbello@gmail.com>
2022-01-13 13:51:33 +0100

    Learn how to debug our code
    code debugging
    - know how to use the required tip to debug code, which is simply: identify, find, fix and prevent.
    - know how to use the simple debugging technic
    - know how to use the complex debugging technic such as adding a debugger kwyword to the breakpoint line

---
+++ src/debug.js
@@ -0,0 +1,40 @@
+// Find Kelvin
+
+// const measureKelvin = () => {
+//   const measurement = {
+//     type: "temp",
+//     unit: "celsius",
+//     // FIX BUG
+//     value: Number(prompt("Enter a number"))
+//   }
+//   debugger
+//   const kelvin = measurement.value + 273
+
+//   // IDENTIFY BUG
+//   console.log(measurement)
+//   // FIND BUG
+//   console.log(typeof measurement.value)
+
+//   return kelvin
+// }
+
+// const result = measureKelvin()
+// console.log(result)
+
+const calcTempsAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2)
+  console.log(temps)
+
+  let min = 0
+  let max = 0
+
+  for (let i = 0; )
+}
+
+const amplitudeBug = calcTempsAmplitudeBug([3, 5, 6, 2], [8, 6, 3, 9, 10]);
+console.log(amplitudeBug)
+
+
+
+
+
